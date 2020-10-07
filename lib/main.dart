aab9d5f69eaebf1c3d65af3892785e3b4db7e814
Amy Roberson <amy.roberson@stablekernel.com>
2020-10-07 15:36:57 -0400

    initializes project with splash screen

---
+++ lib/main.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+import 'package:tutorial/widgets/splash_screen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Reveal Tutorial',
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+      ),
+      home: Splash(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+class Splash extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SplashScreen(
+      seconds: 1,
+      navigateAfterSeconds: MyHomePage(title: "Reveal Tutorial"),
+      title: new Text('Reveal Tutorial'),
+      image: new Image.asset('assets/images/watering_can.png'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
---
+++ lib/widgets/splash_screen.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+import 'dart:core';
+import 'dart:async';
+
+class SplashScreen extends StatefulWidget {
+  final int seconds;
+  final Text title;
+  final dynamic navigateAfterSeconds;
+  final Image image;
+
+  SplashScreen(
+      {
+        @required this.seconds,
+        this.navigateAfterSeconds,
+        this.title = const Text(''),
+        this.image
+      }
+      );
+
+
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Timer(
+        Duration(seconds: widget.seconds),
+            () {
+          if (widget.navigateAfterSeconds is String) {
+            Navigator.of(context).pushReplacementNamed(widget.navigateAfterSeconds);
+          } else if (widget.navigateAfterSeconds is Widget) {
+            Navigator.of(context).pushReplacement(new MaterialPageRoute(builder: (BuildContext context) => widget.navigateAfterSeconds));
+          } else {
+            throw new ArgumentError(
+                'widget.navigateAfterSeconds must either be a String or Widget'
+            );
+          }
+        }
+    );
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: new Stack(
+        fit: StackFit.expand,
+        children: <Widget>[
+          new Container(
+            decoration: new BoxDecoration(
+              color: Colors.green[300],
+            ),
+          ),
+          new Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: <Widget>[
+              new Expanded(
+                flex: 2,
+                child: new Container(
+                    child: new Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        new SizedBox(
+                            height:122.5,
+                            child: widget.image
+                        ),
+                        new Padding(
+                          padding: const EdgeInsets.only(top: 5.0),
+                        ),
+                        widget.title
+                      ],
+                    )),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
