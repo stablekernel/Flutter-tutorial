749c9828729f74274d7b55b566acc1087579dbdf
Amy Roberson <amy.roberson@stablekernel.com>
2020-10-08 15:24:53 -0400

    adds first Navigation Route

--- lib/main.dart
+++ lib/main.dart
@@ -12,16 +12,62 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Reveal Tutorial',
       theme: ThemeData(
-        primaryColor: Colors.green[300],
-        accentColor: Colors.green[900],
+        primarySwatch: Colors.green,
       ),
       home: SplashScreen(
         seconds: 1,
         navigateAfterSeconds: MyHomePage(title: "Reveal Tutorial"),
         title: new Text('Reveal Tutorial'),
-        image: new Image.asset('assets/icon-eucalyptus.png'),
+        image: new Image.asset('assets/images/watering_can.png'),
       ),
       debugShowCheckedModeBanner: false,
     );
   }
 }
+
+// class MyHomePage extends StatefulWidget {
+//   MyHomePage({Key key, this.title}) : super(key: key);
+//
+//   final String title;
+//
+//   @override
+//   _MyHomePageState createState() => _MyHomePageState();
+// }
+//
+// class _MyHomePageState extends State<MyHomePage> {
+//   int _counter = 0;
+//
+//   void _incrementCounter() {
+//     setState(() {
+//       _counter++;
+//     });
+//   }
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text(widget.title),
+//       ),
+//       body: Center(
+//         child: Column(
+//           mainAxisAlignment: MainAxisAlignment.center,
+//           children: <Widget>[
+//             Text(
+//               'You have pushed the button this many times:',
+//             ),
+//             Text(
+//               '$_counter',
+//               style: Theme.of(context).textTheme.headline4,
+//             ),
+//           ],
+//         ),
+//       ),
+//       floatingActionButton: FloatingActionButton(
+//         onPressed: _incrementCounter,
+//         tooltip: 'Increment',
+//         child: Icon(Icons.add),
+//       ),
+//     );
+//   }
+// }
